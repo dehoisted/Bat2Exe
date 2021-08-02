282093c1be2bbe1e04474671ed868ca973c29afc
orlando <75084509+dehoisted@users.noreply.github.com>
2021-08-01 21:39:17 -0400

    Update example.bat

--- Example/example.bat
+++ Example/example.bat
@@ -1,5 +1,10 @@
 rem example.bat
+: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+: Info
 : Example batch file for converting it into an exe.
+: For suggestions, contact be on telegram: https://t.me/Constex
 : If you get any compilation errors, make a new issue at https://github.com/dehoisted/Bat2Exe/issues
 : Huge batch files may fail.
 : Note: comments can be viewed if the exe is decompiled.
