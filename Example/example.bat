454e5c1a6e26323d1a8c96ed163c47cec39c646b
orlando <75084509+dehoisted@users.noreply.github.com>
2021-08-01 20:48:12 -0400

    Added example files

--- Example/example.bat
+++ Example/example.bat
@@ -3,6 +3,7 @@ rem example.bat
 : If you get any compilation errors, make a new issue at https://github.com/dehoisted/Bat2Exe/issues
 : Huge batch files may fail.
 : Note: comments can be viewed if the exe is decompiled.
+
 @echo off
 cls
 title Bat2Exe Example
