9e876d5db1fec89bd5cd65bb6b18971afbcb0013
orlando <75084509+dehoisted@users.noreply.github.com>
2021-08-06 11:56:51 -0400

    Update example.bat

--- Example/example.bat
+++ Example/example.bat
@@ -1,5 +1,5 @@
 rem example.bat
-: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. https://github.com/dehoisted/Bat2Exe
 : THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 : Info
@@ -12,7 +12,7 @@ rem example.bat
 @echo off
 cls
 title Bat2Exe Example
-echo Hello :catkiss:
+echo Hello
 set /p text=Enter text: 
 echo Text entered = %text%
 pause
