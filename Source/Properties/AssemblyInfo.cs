0b510762de38c826ec960749102914bd4825f52f
orlando <75084509+dehoisted@users.noreply.github.com>
2021-09-17 21:52:44 -0400

    Add files via upload

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bat2Exe")]
+[assembly: AssemblyDescription("Converts your batch files into executables.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/dehoisted")]
+[assembly: AssemblyProduct("Bat2Exe")]
+[assembly: AssemblyCopyright("Copyright © 2021 https://github.com/dehoisted/Bat2Exe")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("355fd640-6299-41ea-9e9f-6d10a24effd8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
